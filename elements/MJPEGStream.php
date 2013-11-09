f6754d1e36644daa8f75bc09666d4f3bd8b32a43
jonty-comp <jontysewell@gmail.com>
2013-11-09 23:59:15 +0000

    Make the MJPEG stream reload itself periodically in case the connection dies

--- elements/MJPEGStream.php
+++ elements/MJPEGStream.php
@@ -4,7 +4,13 @@ class MJPEGStream {
 	
 	function output($args) {
 		$id = rand(0,100000);
-		echo("<img src=\"".$args["url"]."\" style=\"width:100%; display: table-cell; vertical-align: center;\"></img>");
+		echo("
+			<script>
+			$().ready(function() {
+				setInterval(function() { $('#".$id."').attr('src', '".$args["url"]."'); }, 60000);
+			})
+			</script>
+			<img id=\"".$id."\" src=\"".$args["url"]."\" style=\"width:100%; display: table-cell; vertical-align: center;\"></img>");
 	}
 
 }
