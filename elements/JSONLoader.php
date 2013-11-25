6234b0a1f8e71e043f0eb8e2c8971c01568369d2
jonty-comp <jontysewell@gmail.com>
2013-11-25 18:20:06 +0000

    Add a CSS parameter for JSON blocks

--- elements/JSONLoader.php
+++ elements/JSONLoader.php
@@ -8,12 +8,13 @@ class JSONLoader extends Element {
 		echo("
 		<style>
 			#json-".$id." { font-weight: bold; font-size: ".$args["font_size"]."em; vertical-align: middle; margin-top: ".$args["margin_top"]."; }
+			".$args["css"]."
 		</style>
 		<script>
 			$().ready(function() {
 				$.getJSON('".$args["url"]."', function(data) {
 					var html = '';
-					
+
 					".$args["callback"]."
 
 					$('#json-".$id."').html(html);
