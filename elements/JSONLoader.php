988aa795e7f62aa6e7d987f2add77637990a5a6c
jonty-comp <jontysewell@gmail.com>
2013-11-25 16:40:01 +0000

    Add JSONLoader element with callback stored in database

---
+++ elements/JSONLoader.php
@@ -0,0 +1,38 @@
+<?php
+
+class JSONLoader extends Element {
+
+	public function output($args) {
+		$id = rand(0,100000);
+
+		echo("
+		<style>
+			#json-".$id." { font-weight: bold; font-size: ".$args["font_size"]."em; vertical-align: middle; margin-top: ".$args["margin_top"]."; }
+		</style>
+		<script>
+			$().ready(function() {
+				$.getJSON('".$args["url"]."', function(data) {
+					var html = '';
+					
+					".$args["callback"]."
+
+					$('#json-".$id."').html(html);
+				});
+				setInterval(function () {
+					$.getJSON('".$args["url"]."', function(data) {
+						var html = '';
+
+						".$args["callback"]."
+
+						$('#json-".$id."').html(html);
+					});
+				}, 5000);
+			});
+		</script>
+		<div id=\"json-".$id."\"></div>
+		");
+	}
+
+}
+
+?>
